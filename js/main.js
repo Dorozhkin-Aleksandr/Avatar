67daed493cbd7d922723da01fa99b04a69e82190
Aleksandr <95400829+Dorozhkin-Aleksandr@users.noreply.github.com>
2022-02-19 23:51:59 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="">',
+        nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="">',
+        responsive: [
+            {
+                breakpoint: 361,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
+                }
+
+            }
+        ]
+
+    });
+});
